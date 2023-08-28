0cbe30461431e92cf91e0de76119444245dfdc95
Begovic Emanulah <ema13bego@gmail.com>
2023-08-28 12:15:50 +0200

    aggiornamento js

--- script.js
+++ script.js
@@ -22,5 +22,29 @@ var parole = [
 ]
 
 var img = [
-    
+    "img/Fig1.png",
+    "img/Fig2.png",
+    "img/Fig3.png",
+    "img/Fig4.png",
+    "img/Fig5.png"
 ]
+
+let divImg = document.querySelector(".imgImpiccato")
+let rnd = Math.floor(Math.random() * 20);
+let text = document.querySelector(".indovinaParola")
+let btn = document.querySelector(".inviaParola");
+let caselleParola = document.querySelector(".caselleParola");
+let parolaNascosta = parole[rnd];
+console.log(parolaNascosta)
+divImg.style.backgroundImage = `url("${img[0]}")`
+
+
+btn.addEventListener("click", function(){
+    if(text.value.toLowerCase() == parolaNascosta){
+        console.log("indovinato")
+    }
+    else(console.log("no"))
+})
+
+
+
