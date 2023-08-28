701c513da51df84d046c551e17d5893f3922f680
Begovic Emanulah <ema13bego@gmail.com>
2023-08-28 11:46:48 +0200

    aggiunta 5 immagini

---
+++ script.js
@@ -0,0 +1,26 @@
+var parole = [
+    "sommozzatore",
+    "massaggiatrice",
+    "marca",
+    "prigione",
+    "tubo",
+    "commento",
+    "automatico",
+    "furto",
+    "piedistallo",
+    "farina",
+    "carpa",
+    "srotolare",
+    "indirizzo",
+    "plastica",
+    "smalto",
+    "sud",
+    "randagi",
+    "artisti",
+    "fragile",
+    "argilla"
+]
+
+var img = [
+    
+]
